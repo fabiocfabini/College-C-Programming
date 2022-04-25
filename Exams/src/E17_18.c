ba1995d34f1d1957a6700da2fed7ce74288a97a6
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-04-26 00:07:31 +0100

    added solution to Exam 17-18

---
+++ Exams/src/E17_18.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "E17_18.h"
+
+// Helper functions
+Palavras newPalavra(char *palavra, int comp, Palavras rest)
+{
+    Palavras new = malloc(sizeof(struct celula));
+
+    if (new)
+    {
+        new->palavra = palavra;
+        new->comp = comp;
+        new->prox = rest;
+    }
+
+    return new;
+}
+
+void printPalavras(Palavras p)
+{
+    while (p)
+    {
+        for (int i = 0; i < p->comp; i++)
+        {
+            printf("%c", *(p->palavra + i));
+        }
+        printf(",%d", p->comp);
+        p = p->prox;
+        printf(" -> ");
+    }
+    printf("NULL\n");
+}
+
+void freePalavras(Palavras p)
+{
+    while (p)
+    {
+        Palavras temp = p->prox;
+        free(temp);
+        p = temp;
+    }
+}
+
+// Exam Questions
+int daPalavra(char *s, int *e)
+{
+    int space_count = 0;
+    while (*s && isspace(*s))
+    {
+        space_count++;
+        s++;
+    }
+    *e = space_count;
+
+    int word_count = 0;
+    while (*s && !isspace(*s))
+    {
+        word_count++;
+        s++;
+    }
+
+    return word_count;
+}
+
+Palavras words(char *texto)
+{
+    Palavras out = NULL;
+    Palavras *ad = &out;
+    int space_count, word_length;
+
+    while (*texto)
+    {
+        word_length = daPalavra(texto, &space_count);
+        if (word_length > 0)
+        {
+            Palavras new = newPalavra(texto + space_count, word_length, *ad);
+            *ad = new;
+            ad = &(*ad)->prox;
+        }
+        texto += space_count + word_length;
+    }
+
+    return out;
+}
+
+Palavras daLinha(Palavras t, int n)
+{
+    Palavras out = NULL;
+    Palavras *ad = &t;
+    if (*ad && (*ad)->comp > n)
+    {
+        out = (*ad)->prox;
+        (*ad)->prox = NULL;
+        n = 0;
+    }
+    while (*ad && n > 0)
+    {
+        n -= (*ad)->comp + 1 /*for extra space char*/;
+        if (n < 0)
+        {
+            out = *ad;
+            *ad = NULL;
+        }
+        else if (n == 0)
+        {
+            ad = &(*ad)->prox;
+            out = *ad;
+            *ad = NULL;
+        }
+        else
+        {
+            ad = &(*ad)->prox;
+        }
+    }
+    return out;
+}
+
+int countWordsAndChars(Palavras p, int *nChars)
+{
+    int c = 0;
+    *nChars = 0;
+    while (p)
+    {
+        *nChars += p->comp;
+        p = p->prox;
+        c++;
+    }
+    return c;
+}
+
+void escreveLinha(Palavras p, int n)
+{
+    int total_chars, total_words, total_spaces, spaces_per_word, remainder;
+    total_words = countWordsAndChars(p, &total_chars);
+    total_spaces = n - total_chars;
+    spaces_per_word = total_spaces / (total_words - 1);
+    remainder = total_spaces % (total_words - 1);
+
+    // As long as there are words in p
+    while (p)
+    {
+        // print the words
+        for (int i = 0; i < p->comp; i++)
+        {
+            printf("%c", *(p->palavra + i));
+        }
+        // if next word exists
+        if (p->prox)
+        {
+            // print the spaces
+            for (int i = 0; i < spaces_per_word + (0 < remainder); i++)
+            {
+                printf(" ");
+            }
+            remainder--;
+        }
+        // free the printed word
+        Palavras temp = p->prox;
+        free(p);
+        p = temp;
+    }
+    printf("\n");
+}
+
+void formata(char texto[], int largura)
+{
+    Palavras p = words(texto);
+    while (p)
+    {
+        Palavras aux = daLinha(p, largura);
+        escreveLinha(p, largura);
+        p = aux;
+    }
+}
--- Exams/src/main.c
+++ Exams/src/main.c
@@ -2,146 +2,146 @@
 #include <stdlib.h>
 #include "E15_16.h"
 #include "E16_17.h"
+#include "E17_18.h"
 #include "R14_15.h"
 #include "R15_16.h"
 
-int main(int argc, char** argv){
-    
+int main(int argc, char **argv)
+{
+    formata(big_text, 100);
 
     return 0;
 }
 
 //* R15_16
-    // char* words[TOP];
-    // int word_count[TOP], i = 0, hist_length = 0;
-    // char word[MAX_WORD_SIZE];
-    // Hist head = NULL;
+// char* words[TOP];
+// int word_count[TOP], i = 0, hist_length = 0;
+// char word[MAX_WORD_SIZE];
+// Hist head = NULL;
 
-    // printf("Write something:\n");
+// printf("Write something:\n");
 
-    // do
-    // {
-    //     scanf ("%60[^ \n]s", word);
-    //     filter(word);
-    //     hist_length += inc(&head, word);
+// do
+// {
+//     scanf ("%60[^ \n]s", word);
+//     filter(word);
+//     hist_length += inc(&head, word);
 
-    // } while (getchar() != '\n');
+// } while (getchar() != '\n');
 
+// if(hist_length > TOP) hist_length = TOP;
+// for(; i < hist_length; i++){
+//     words[i] = remMaisFreq(&head, word_count+i);
+// }
 
-    // if(hist_length > TOP) hist_length = TOP;
-    // for(; i < hist_length; i++){
-    //     words[i] = remMaisFreq(&head, word_count+i);
-    // }
+// for (i = 0; i < hist_length; i++){
+//     printf("[%d] -> %s, %d\n", i+1, words[i], word_count[i]);
+// }
 
-    // for (i = 0; i < hist_length; i++){
-    //     printf("[%d] -> %s, %d\n", i+1, words[i], word_count[i]);
-    // }
-
-    // freeHist(head);
-    // printf("\nEND\n");
+// freeHist(head);
+// printf("\nEND\n");
 
 //* R14_15
-    // Alunos root   = newAluno("fabio", 1000, 14, NULL, NULL);
-    // Alunos rootl  = newAluno("lucas",  900,  2, NULL, NULL);
-    // Alunos rootll = newAluno("luisa",  800, 19, NULL, NULL);
-    // Alunos rootlr = newAluno("pedro",  950, 18, NULL, NULL);
-    // Alunos rootr  = newAluno("maria", 1100, 14, NULL, NULL);
-    // Alunos rootrl = newAluno("sofia", 1050, 18, NULL, NULL);
-    // Alunos rootrr = newAluno("santa", 1200, 20, NULL, NULL);
-    // root->esq = rootl; root->dir = rootr;
-    // rootl->esq = rootll; rootl->dir = rootlr;
-    // rootr->esq = rootrl; rootr->dir = rootrr;
+// Alunos root   = newAluno("fabio", 1000, 14, NULL, NULL);
+// Alunos rootl  = newAluno("lucas",  900,  2, NULL, NULL);
+// Alunos rootll = newAluno("luisa",  800, 19, NULL, NULL);
+// Alunos rootlr = newAluno("pedro",  950, 18, NULL, NULL);
+// Alunos rootr  = newAluno("maria", 1100, 14, NULL, NULL);
+// Alunos rootrl = newAluno("sofia", 1050, 18, NULL, NULL);
+// Alunos rootrr = newAluno("santa", 1200, 20, NULL, NULL);
+// root->esq = rootl; root->dir = rootr;
+// rootl->esq = rootll; rootl->dir = rootlr;
+// rootr->esq = rootrl; rootr->dir = rootrr;
 
-    // int freq[21];
-    // for (int i = 0; i < 21; freq[i++] = 0);
-    // int N = fnotas(root, freq);
-    // printf("There are %d students.\n\n", N);
-    
-    // printf("Rank of %s is %d.\n\n", root->nome, rankAluno(root, root->numero, freq));
-    
-    // StrList nota14;
-    // int n = comNota(root, 14, &nota14);
-    // printf("%d Students with 14: ", n);
-    // StrList aux = nota14;
-    // while(aux){ 
-    //     printf("%s, ", aux->string);
-    //     aux = aux->prox;
-    // } printf("\n\n");
-    // freeStrList(nota14);
+// int freq[21];
+// for (int i = 0; i < 21; freq[i++] = 0);
+// int N = fnotas(root, freq);
+// printf("There are %d students.\n\n", N);
 
-    // printf("ClassRoom:\n");
-    // imprime(root);
+// printf("Rank of %s is %d.\n\n", root->nome, rankAluno(root, root->numero, freq));
 
-    // freeAlunos(root);
+// StrList nota14;
+// int n = comNota(root, 14, &nota14);
+// printf("%d Students with 14: ", n);
+// StrList aux = nota14;
+// while(aux){
+//     printf("%s, ", aux->string);
+//     aux = aux->prox;
+// } printf("\n\n");
+// freeStrList(nota14);
+
+// printf("ClassRoom:\n");
+// imprime(root);
+
+// freeAlunos(root);
 
 //* E15_16
-    // Colunas col0 = newCol(0, 2, newCol(2, 3, newCol(3, 4, newCol(4, 5, NULL))));
-    // Colunas col1 = newCol(1, -2, newCol(4, 1, newCol(5, 4, NULL)));
-    // Colunas col2 = newCol(2, 2, NULL);
-    // Colunas col3 = newCol(0, -1, newCol(1, 4, newCol(3, 1, newCol(4, 9, NULL))));
-    // Colunas _col0 = newCol(0, 2, newCol(2, 3, newCol(3, 4, newCol(4, 5, NULL))));
-    // Colunas _col1 = newCol(1, -2, newCol(4, 1, newCol(5, 4, NULL)));
-    // Colunas _col2 = newCol(2, 2, NULL);
-    // Colunas _col3 = newCol(0, -1, newCol(1, 4, newCol(3, 1, newCol(4, 9, NULL))));
+// Colunas col0 = newCol(0, 2, newCol(2, 3, newCol(3, 4, newCol(4, 5, NULL))));
+// Colunas col1 = newCol(1, -2, newCol(4, 1, newCol(5, 4, NULL)));
+// Colunas col2 = newCol(2, 2, NULL);
+// Colunas col3 = newCol(0, -1, newCol(1, 4, newCol(3, 1, newCol(4, 9, NULL))));
+// Colunas _col0 = newCol(0, 2, newCol(2, 3, newCol(3, 4, newCol(4, 5, NULL))));
+// Colunas _col1 = newCol(1, -2, newCol(4, 1, newCol(5, 4, NULL)));
+// Colunas _col2 = newCol(2, 2, NULL);
+// Colunas _col3 = newCol(0, -1, newCol(1, 4, newCol(3, 1, newCol(4, 9, NULL))));
 
-    // Mat m = newLine(0, col0, newLine(2, col1, newLine(3, col2, newLine(5, col3, NULL))));
-    // Mat m1 = newLine(0, _col0, newLine(2, _col1, newLine(3, _col2, newLine(5, _col3, NULL))));
+// Mat m = newLine(0, col0, newLine(2, col1, newLine(3, col2, newLine(5, col3, NULL))));
+// Mat m1 = newLine(0, _col0, newLine(2, _col1, newLine(3, _col2, newLine(5, _col3, NULL))));
 
-    // printMat(m);
+// printMat(m);
 
-    // int line = 6, col = 8, value = 7;
-    // printf("\nMatrix entry at line %d column %d: %g\n\n", line, col, getEntry(m, line, col));
+// int line = 6, col = 8, value = 7;
+// printf("\nMatrix entry at line %d column %d: %g\n\n", line, col, getEntry(m, line, col));
 
-    // printf("Setting entry at line %d column %d equal to %d.\n", line, col, value);
+// printf("Setting entry at line %d column %d equal to %d.\n", line, col, value);
 
-    // setEntry(&m, line, col, value);
+// setEntry(&m, line, col, value);
 
-    // printf("\nMatrix entry at line %d column %d: %g\n\n", line, col, getEntry(m, line, col));
+// printf("\nMatrix entry at line %d column %d: %g\n\n", line, col, getEntry(m, line, col));
 
-    // printMat(m);
+// printMat(m);
 
-    // printf("\nTransposing matrix m\n");
+// printf("\nTransposing matrix m\n");
 
-    // transpose(&m);
+// transpose(&m);
 
-    // printMat(m);
+// printMat(m);
 
-    // printf("\nAdding m1 to new m\n");
-    // addTo(&m, m1);
-    // printMat(m);
+// printf("\nAdding m1 to new m\n");
+// addTo(&m, m1);
+// printMat(m);
 
-    // freeMat(m);
-    // freeMat(m1);
-
+// freeMat(m);
+// freeMat(m1);
 
 //* E16_17
-    // int N = 10;
-    // StackC s;
-    // CList c = newCList(NULL);
-    
-    // initStackC(&s, c);
+// int N = 10;
+// StackC s;
+// CList c = newCList(NULL);
 
-    // printf("\t");printStackC(s);
-    // for (int i = 0; i < N; i++){
-    //     push(&s, i);
-    //     printf("Pushed %d to StackC\n", i);
-    //     printf("\t");printStackC(s);
-    // }
+// initStackC(&s, c);
 
-    // printf("\nThe Stack has %d elements\n\n", size(s));
-    
-    // printf("Inverting StackC with pop push\n\t");
-    // Libreverse(&s);
-    // printStackC(s);
-    // printf("Inverting StackC w/out pop push\n\t");
-    // Libreverse(&s);
-    // printStackC(s);
+// printf("\t");printStackC(s);
+// for (int i = 0; i < N; i++){
+//     push(&s, i);
+//     printf("Pushed %d to StackC\n", i);
+//     printf("\t");printStackC(s);
+// }
 
-    // int x;
-    // for (int i = 0; i < N + 1; i++){
-    //     if(pop(&s, &x) == 0) printf("Removed %d from StackC.\n", x);
-    //     else printf("Empty StackC\nbin");
-    //     printf("\t");printStackC(s);
-    // }
+// printf("\nThe Stack has %d elements\n\n", size(s));
 
-    // if(s.valores) freeCList(s.valores); 
+// printf("Inverting StackC with pop push\n\t");
+// Libreverse(&s);
+// printStackC(s);
+// printf("Inverting StackC w/out pop push\n\t");
+// Libreverse(&s);
+// printStackC(s);
+
+// int x;
+// for (int i = 0; i < N + 1; i++){
+//     if(pop(&s, &x) == 0) printf("Removed %d from StackC.\n", x);
+//     else printf("Empty StackC\nbin");
+//     printf("\t");printStackC(s);
+// }
+
+// if(s.valores) freeCList(s.valores);
